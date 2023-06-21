73ceff206cccf77cbe837a79c2abbd542b341921
Luis Melendez <ancientlim00@gmail.com>
2023-06-21 16:31:12 -0500

    some css and responsive fixes on icon-chart

--- src/app/Modules/Components/icon-chart/icon-chart.component.ts
+++ src/app/Modules/Components/icon-chart/icon-chart.component.ts
@@ -15,20 +15,20 @@ export class IconChartComponent {
   @Output() dataToDelete = new EventEmitter<any>();
 
 
-  constructor(private modal: MatDialog,){}
+  constructor(private modal: MatDialog,) { }
 
-  @Input() view: IconChart[] = [] 
+  @Input() view: IconChart[] = []
 
 
   openModalUpdate(item: IconChart) {
     this.dataToUpdate.emit(item)
   }
 
-  deleteItem(itemID: number , itemName: string){
-    this.dataToDelete.emit({itemId:itemID,itemName:itemName})
+  deleteItem(itemID: number, itemName: string) {
+    this.dataToDelete.emit({ itemId: itemID, itemName: itemName })
   }
 
-  page_size: number = 10;
+  page_size: number = 1;
   page_number: number = 1;
 
   handlePage(e: PageEvent) {
@@ -38,25 +38,29 @@ export class IconChartComponent {
 
   @HostListener('window:resize')
 
+  ngOnInit() {
+    this.cambiarVariable()
+  }
+
   onWindowResize() {
-    this.cambiarVariable(); 
+    this.cambiarVariable();
   }
   cambiarVariable() {
     const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-   if (screenWidth > 1633 ) {
+    if (screenWidth > 1633) {
       this.page_size = 10;
     }
-    else if (screenWidth < 1633 && screenWidth >=1314 ){
+    else if (screenWidth < 1633 && screenWidth >= 1314) {
       this.page_size = 8;
     }
     else if (screenWidth < 1314 && screenWidth >= 995) {
       this.page_size = 6;
     }
-    else if (screenWidth < 995 && screenWidth >= 800 ) {
+    else if (screenWidth < 995 && screenWidth >= 800) {
       this.page_size = 4;
     }
-    else if (screenWidth < 800 && screenWidth >= 675 ) {
+    else if (screenWidth < 800 && screenWidth >= 675) {
       this.page_size = 2;
     }
     else if (screenWidth < 675) {
